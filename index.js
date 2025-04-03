38b146daee1890df9b2c98097792e1af0089d1f4
sanarsk <sanarassak@gmail.com>
2025-04-03 17:11:31 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 let display = document.getElementById("display");
 
-let buttons = Array.from(document.getElementByclassName('button'));
+let buttons = Array.from(document.getElementsByClassName('button'));
 
-button.map (buttons =>{
-    buttons.addEventListener ('click',(e)=>{
-        switch(e.target.innerText){
-            case'c':
-    display.innerText='';
-    break;
-    case'<=':
-    if (display.innerText){
-    display.innerText= display.innerText.slice(0,-1);
-        }
-    break;
-case'=':
-display.innerText=eval(display.innerText);
+buttons.map(button => {
+    console.log("tap")
+    button.addEventListener('click', (e) => {
+        switch (e.target.innerText) {
+            case 'C':
+                display.innerText = '';
+                break;
+            case '←':
+                if (display.innerText) {
+                    display.innerText = display.innerText.slice(0, -1);
+                }
+                break;
+            case '=':
+
+                display.innerText = eval(display.innerText);
             default:
 
-                display.innerText += e.target.innerText;
- } })
-    });
+                display.innerText += e.target.innerText
+        }
+    })
+})
 
-        
+
